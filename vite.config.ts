f77ec8b3d63faac94e8d0968b5eee98a30da2ca8
David Secretan-Hallett <david.secretan-hallett@csiro.au>
2025-01-31 11:23:35 +1100

    Added plotly as an external dependency to vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -18,13 +18,14 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['geotiff', 'leaflet'],
+      external: ['geotiff', 'leaflet', 'plotly'],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
             geotiff: 'geotiff',
-            leaflet: 'leaflet'
+            leaflet: 'leaflet',
+            plotly: 'plotly'
         },
       },
     },
